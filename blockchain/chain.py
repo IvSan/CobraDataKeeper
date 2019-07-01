240be458e6c2c43d29d55d031c808326c7d655a5
IvSan <foritun@gmail.com>
2019-07-01 12:27:23 +0300

    making genesis block

--- blockchain/chain.py
+++ blockchain/chain.py
@@ -10,24 +10,30 @@ class Chain:
 
     def __init__(self):
         self.current_data = []
-        self.chain = []
+        genesis = Block(0, 0, 0)
+        self.chain = [genesis]
+        print(genesis, 'Genesis\n')
 
     def add_block(self):
         index = len(self.chain)
-        previous_block_hash = self.chain[-1].hash() if self.chain else 0
-        proof = Chain.find_proof(self.chain[-1]) if self.chain else 0
+        previous_block_hash = self.chain[-1].hash()
+        proof, tries = Chain.find_proof(self.chain[-1])
 
         block = Block(index, previous_block_hash, proof)
         self.chain.append(block)
+        print(block,
+              'Work amount: ' + str(tries) + '\n\n')
 
     @staticmethod
     def find_proof(last_block):
+        tries = 0
         while True:
+            tries += 1
             proof = str(uuid4())
             hash_to_check = sha256()
             hash_to_check.update(last_block.hash().encode() + proof.encode())
             if int(hash_to_check.hexdigest(), 16) <= Chain.TARGET:
-                return proof
+                return proof, tries
 
     def __str__(self):
         result = ''
@@ -40,4 +46,4 @@ chain = Chain()
 chain.add_block()
 chain.add_block()
 chain.add_block()
-print(chain)
+# print(chain)
